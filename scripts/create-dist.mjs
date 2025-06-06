9b5416e9581944c4b6a13421f3731c7595952633
fmoloshnikov <fmoloshnikov@gmail.com>
2025-06-06 19:17:10 +0300

    [SJG-35] add dist package on build

---
+++ scripts/create-dist.mjs
@@ -0,0 +1,12 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+const distPackagePath = path.resolve(__dirname, "../package.dist.json");
+const distDirectoryPath = path.resolve(__dirname, "../dist/package.json");
+fs.copyFileSync(distPackagePath, distDirectoryPath);
