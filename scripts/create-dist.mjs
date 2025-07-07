a20cf8ca7105d7d77fbfcd642106241a7f6f285d
fmoloshnikov <fmoloshnikov@gmail.com>
2025-07-07 15:47:28 +0300

    copy version from source package.json

--- scripts/create-dist.mjs
+++ scripts/create-dist.mjs
@@ -2,11 +2,15 @@ import fs from "fs";
 import path, { dirname } from "path";
 import { fileURLToPath } from 'url';
 
+function createDistJson() {
+  const sourceJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../package.json")));
+  const distJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../package.dist.json")));
+  distJson.version = sourceJson.version;
+  fs.writeFileSync(path.resolve(__dirname, "../dist/package.json"), JSON.stringify(distJson, null, 2));
+}
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
-const distPackagePath = path.resolve(__dirname, "../package.dist.json");
-const distDirectoryPath = path.resolve(__dirname, "../dist/package.json");
-fs.copyFileSync(distPackagePath, distDirectoryPath);
+createDistJson();
 fs.copyFileSync(path.resolve(__dirname, "../.npmrc"), path.resolve(__dirname, "../dist/.npmrc"));
