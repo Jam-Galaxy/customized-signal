5032ff1140356c2824d471c720de117928122e82
fmoloshnikov <fmoloshnikov@gmail.com>
2025-07-07 14:11:27 +0300

    update dist

--- scripts/create-dist.mjs
+++ scripts/create-dist.mjs
@@ -1,7 +1,6 @@
 import fs from "fs";
-import path from "path";
+import path, { dirname } from "path";
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,3 +9,4 @@ const __dirname = dirname(__filename);
 const distPackagePath = path.resolve(__dirname, "../package.dist.json");
 const distDirectoryPath = path.resolve(__dirname, "../dist/package.json");
 fs.copyFileSync(distPackagePath, distDirectoryPath);
+fs.copyFileSync(path.resolve(__dirname, "../.npmrc"), path.resolve(__dirname, "../dist/.npmrc"));
